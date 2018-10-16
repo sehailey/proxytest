72a63a671cd190486a74baa2ef9f58bcd5d5927a
sehailey <sarah.elizabeth.hailey@gmail.com>
2018-10-16 19:23:24 -0400

    tryint to add console.logs

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
-    this.state = { data: "Did not get data from Express", error: "" };
+    this.state = {
+      data: "Did not get data from Express",
+      error: "",
+      eee: "",
+      text: ""
+    };
   }
   componentDidMount() {
     this.fetchExpress();
   }
 
   fetchExpress = async () => {
+    const eee = await fetch(`/api`).then(res => console.log(res));
+    console.log(eee);
     try {
       const response = await fetch(`/api`);
+      const text = await response.text();
+      console.log(text);
       const { data } = await response.json();
       console.log("data", data);
       this.setState({ data: data });
@@ -30,6 +39,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>WELCOME CREATE REACT APP!</p>
           <div className="App-link">{this.state.data}</div>
+          <div>error: {this.state.error}</div>
+          <div>text: {this.state.text}</div>
+          <div>eee: {this.state.eee}</div>
         </header>
       </div>
     );
