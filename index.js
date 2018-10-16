530d610d31dc37998f2683d1cb3091fd6f7604d6
sehailey <sarah.elizabeth.hailey@gmail.com>
2018-10-16 17:08:33 -0400

    FUUUUUUUUU

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const path = require("path");
+
+// Create the server
+const app = express();
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "build")));
+
+// Serve our api route /cow that returns a custom talking text cow
+app.get("/api", async (req, res, next) => {
+  try {
+    res.json({ data: "HELLOOOOO FROM EXPRESS" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Anything that doesn't match the above, send back the index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Mixing it up on port ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  componentDidMount() {
+    this.fetchExpress();
+  }
+
+  fetchExpress = async () => {
+    const response = await fetch(`/api`);
+    const data = await response.json();
+
+    this.setState(data);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>WELCOME CREATE REACT APP!</p>
+          <div className="App-link">{this.state.data}</div>
         </header>
       </div>
     );
